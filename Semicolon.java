9abc034408504c09c4b6576ab7f3fa3996ddbdc3
Dilip pashi <dilip.pashi143@gmail.com>
2016-10-05 12:06:39 +0530

    AcadGild
    Java assignment 2.1

---
+++ Semicolon.java
@@ -0,0 +1,11 @@
+// Program to print your name without using semicolon in the program
+
+class Semicolon {
+	
+	public static void main(String[] args) {
+		if (System.out.printf("Dilip Pashi") != null){
+			
+		}
+
+}
+}
